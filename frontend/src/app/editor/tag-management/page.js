3f1332f226e2e70a83f8f6b8306b1abeb77492fa
angelinanqi <sohangelina2018@gmail.com>
2025-11-03 09:10:26 +0800

    feat(tags): edit tags

--- frontend/src/app/editor/tag-management/page.js
+++ frontend/src/app/editor/tag-management/page.js
@@ -1,17 +1,21 @@
 "use client";
 
 import EditorLayout from "../../../components/layouts/EditorLayout.jsx";
-import { Heading, Button, Table, Stack, Box } from "@chakra-ui/react";
+import { Heading, Button, Table, Stack, Box, Input } from "@chakra-ui/react";
 import { toaster } from "../../../styles/ui/toaster.jsx";
 import AddTag from "../../../components/AddTag.jsx";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-//TODO: when delete or add tag, reflect that in the ui immediately instead of reload
+//TODO: when add tag, reflect that in the ui immediately instead of reload
+//TODO: when input value in edit tag mode is not changed/same, disable save
 
 export default function TagManagement() {
   const BASE_API_URL = "http://127.0.0.1:8000/tags/";
   const [tags, setTags] = useState([]);
+
+  const [editingID, setEditingID] = useState(null);
+  const [editingTitle, setEditingTitle] = useState("");
 
   useEffect(() => {
     async function fetchTags() {
@@ -29,12 +33,45 @@ export default function TagManagement() {
   async function handleDeleteTags(tag_id) {
     try {
       await axios.delete(BASE_API_URL + tag_id + "/");
+      setTags((prevTags) => prevTags.filter((tag) => tag.id !== tag_id));
       toaster.create({
         description: "Tag deleted successfully!",
         duration: 3000,
       });
     } catch (error) {
       console.error("Error:", error);
+    }
+  }
+
+  function selectEdit(tag) {
+    setEditingID(tag.id);
+    setEditingTitle(tag.title);
+  }
+
+  async function handleSaveEdit(tag_id) {
+    const updated_title = editingTitle;
+    try {
+      await axios.put(BASE_API_URL + tag_id + "/", { title: updated_title });
+
+      setTags((prevTags) =>
+        prevTags.map((tag) =>
+          tag.id === tag_id ? { ...tag, title: editingTitle } : tag
+        )
+      );
+
+      toaster.create({
+        description: "Tag updated!",
+        duration: 3000,
+      });
+
+      setEditingID(null);
+      setEditingTitle("");
+    } catch (error) {
+      console.error("Error", error);
+      toaster.create({
+        description: "Error updating tag!",
+        duration: 3000,
+      });
     }
   }
 
@@ -66,20 +103,59 @@ export default function TagManagement() {
             {tags.length > 0 ? (
               tags.map((tag) => (
                 <Table.Row key={tag.id}>
-                  <Table.Cell>{tag.title}</Table.Cell>
+                  <Table.Cell>
+                    {editingID === tag.id ? (
+                      <Input
+                        size="sm"
+                        value={editingTitle}
+                        onChange={(e) => setEditingTitle(e.target.value)}
+                        width="auto"
+                      />
+                    ) : (
+                      tag.title
+                    )}
+                  </Table.Cell>
                   <Table.Cell textAlign="center">0</Table.Cell>
                   <Table.Cell textAlign="center">
-                    <Button size="xs" bg="gray.400">
-                      Edit
-                    </Button>
-                    <Button
-                      size="xs"
-                      bg="red.600"
-                      marginLeft={"10px"}
-                      onClick={() => handleDeleteTags(tag.id)}
-                    >
-                      Delete
-                    </Button>
+                    {editingID === tag.id ? (
+                      <>
+                        <Button
+                          size="xs"
+                          bg="green.600"
+                          onClick={() => handleSaveEdit(tag.id)}
+                        >
+                          Save
+                        </Button>
+
+                        <Button
+                          size="xs"
+                          bg="gray.400"
+                          marginLeft={"10px"}
+                          onClick={() => setEditingID(null)}
+                        >
+                          Cancel
+                        </Button>
+                      </>
+                    ) : (
+                      <>
+                        <Button
+                          size="xs"
+                          bg="gray.400"
+                          onClick={() => selectEdit(tag)}
+                        >
+                          Edit
+                        </Button>
+
+                        <Button
+                          size="xs"
+                          bg="red.600"
+                          marginLeft={"10px"}
+                          onClick={() => handleDeleteTags(tag.id)}
+                        >
+                          Delete
+                        </Button>
+                      </>
+                    )}
                   </Table.Cell>
                 </Table.Row>
               ))
