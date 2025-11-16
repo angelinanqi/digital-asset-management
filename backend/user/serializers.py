70ac963d3fc92a05a7af8ea46ecfe0617c9ae0b4
angelinanqi <sohangelina2018@gmail.com>
2025-11-16 21:09:42 +0800

    feature: registration field adjustment

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,11 +1,40 @@
 from django.contrib.auth.models import Group
 from .models import User
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
-        fields = ["id", "url", "username", "first_name", "last_name", "password", "email", "groups", "is_active", "date_joined"]
+        fields = ["id", "url", "username", "first_name", "last_name", "password1", "password2", "email", "groups", "is_active", "date_joined"]
+        extra_kwargs = {'password': {'write_only':True},
+                        'is_active': {'read_only':True},
+                        'date_joined': {'read_only':True},
+                        }
+
+    def validate(self, value):
+        if value['password1'] != value['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        else:
+            try:
+                validate_password(value['password1'])  # runs all validators in settings
+            except ValidationError as e:
+                raise serializers.ValidationError(list(e.messages))
+            return value
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password1')
+        validated_data.pop('password2')
+        groups_data = validated_data.pop('groups',[])
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        user.groups.set(groups_data)
+        return user
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
