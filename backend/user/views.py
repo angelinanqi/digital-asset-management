912c0cb08a9e054220f984d1754da83542fbbde9
angelinanqi <sohangelina2018@gmail.com>
2025-11-17 02:17:36 +0800

    feature: finish registration

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import Group
 from .models import User
 from .serializers import UserSerializer, GroupSerializer
 from rest_framework import permissions, viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,6 +15,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [AllowAny()]
+        return [IsAdminUser()]
+    
 
     
 
--- frontend/src/components/users/SignUpForm.jsx
+++ frontend/src/components/users/SignUpForm.jsx
@@ -11,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 import { PasswordInput } from "@/components/ui/password-input";
 import { useState } from "react";
+import axios from "axios";
 
 export default function SignUpForm() {
   const [error, setError] = useState(""); // store error message
@@ -18,22 +19,50 @@ export default function SignUpForm() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
 
-  const handleRegister = async () => {
-    if (!username || !firstName || !lastName || !email || !password) {
+  const BASE_API_URL = "http://127.0.0.1:8000/users/";
+
+  async function handleRegister() {
+    if (
+      !username ||
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password1 ||
+      !password2
+    ) {
       setError("Please fill in all fields!");
       return;
     }
 
+    const signUpFormData = new FormData();
+
+    signUpFormData.append("username", username);
+    signUpFormData.append("first_name", firstName);
+    signUpFormData.append("last_name", lastName);
+    signUpFormData.append("email", email);
+    signUpFormData.append("password1", password1);
+    signUpFormData.append("password2", password2);
+
     try {
-      // const res = await axios.post("/api/register", { username, firstName, lastName, email, password });
+      console.log("touch me bb");
+      await axios.post(BASE_API_URL, {
+        username,
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        password1,
+        password2,
+      });
+      console.log("touch me boy");
       setError(""); // clear error on success
       alert("Registration successful!");
     } catch (err) {
       setError("Registration failed. Please try again.");
     }
-  };
+  }
 
   return (
     <Center h="100vh">
@@ -100,8 +129,17 @@ export default function SignUpForm() {
             <Field.Root>
               <Field.Label>Password</Field.Label>
               <PasswordInput
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={password1}
+                onChange={(e) => setPassword1(e.target.value)}
+                borderColor="gray.300"
+              />
+            </Field.Root>
+
+            <Field.Root>
+              <Field.Label>Confirm Password</Field.Label>
+              <PasswordInput
+                value={password2}
+                onChange={(e) => setPassword2(e.target.value)}
                 borderColor="gray.300"
               />
             </Field.Root>
