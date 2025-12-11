0752338e617bd12f51e97087e226d91abb1e45a1
angelinanqi <sohangelina2018@gmail.com>
2025-12-11 17:10:22 +0800

    fix(preview): update toaster msg

--- frontend/src/components/UpdateAssetModal.jsx
+++ frontend/src/components/UpdateAssetModal.jsx
@@ -57,7 +57,7 @@ export default function UpdateAssetModal({ asset }) {
       });
 
       toaster.create({
-        description: "Successfully Updated Only Asset File!",
+        description: "Successfully updated this file!",
         type: "info"
       });
 
